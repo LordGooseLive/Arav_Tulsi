ae1ee20d31f27bc956e6322c8944e4715eaf5a5f
LordGooseLive <aravtulsi@gmail.com>
2024-02-16 11:22:19 -0500

    test comment

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -8,16 +8,20 @@ typedef struct node {
 
 // Returns number of nodes in the linkedList.
 int length(node* head)
-{
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+{	
+	//Var declaration
+	int len = 0; //length of list
 
-   return (len);
+	//counts nodes
+	if (head != NULL) //ensures list exists
+	{
+		//itterates through list
+		for (len; head-> next != NULL; len++) // local <<head>> safe to increment
+		{
+			head = head -> next; //itterates <<head>>
+		}
+	}
+	return len; //returns length of list
 }
 
 // parses the string in the linkedList
@@ -25,6 +29,28 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+	//Var declaration
+	int len = length (head); //gets length of list	
+	node * temp = head; //traversial variable
+	char * string = (char *) malloc (sizeof (char ) * (++len)); //dynamic string
+	/* Incrementing <<len>> above 
+	allows for null char and reduces 
+	operations in string traversal 
+	and manipulation in following 
+	lines of code*/
+	
+	//Parses string
+	if (string != NULL)	//ensures malloc successful
+	{
+		//traverses lists and array
+		for (int i = 0; i < len; i++)
+		{
+			string[i] = temp->letter; //copies list to array
+			temp = temp->next; //travels to next node
+		}
+		string [len] = '\0'; //initialises last element to NULL
+	}
+	return string;
 }
 
 // inserts character to the linkedlist
@@ -33,11 +59,43 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+	//creates and initialises newnode
+	node * newNode = (node*) malloc (sizeof (node));
+	newNode-> letter = c; //stores passby-char
+	newNode-> next = NULL; //marks as list end
+
+	//inserts node to empty list
+	if ( *pHead == NULL)
+	{
+		*pHead = newNode; //links new node
+	}
+
+	//inserts node to existing list
+	else
+	{
+		node *temp = *pHead; //temporary variable improves readability
+		for (temp; temp->next != NULL; temp = temp->next); //traverses to tail
+		temp->next = newNode; //links new node
+	}
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+	//Var declaration
+	node * current = *pHead;
+	node * temp = NULL;
+
+	//deallocation process
+	while (current != NULL) //traverses through list
+	{
+		temp = current;
+		current = current->next; //itterates current
+		free (temp); //frees node
+	}
+
+	//clears head pointer in main
+	*pHead = NULL; 
 }
 
 int main(void)
